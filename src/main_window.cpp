8e3070a99e5690dd100f36864037f2bf3d73165f
Luka Sokolov <lukasokolov@gmail.com>
2019-11-08 15:53:43 +0100

    Added files for styling up ui. In addition, changed the default font of the application.

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -2,12 +2,20 @@
 #include "main_window.hpp"
 #include "ui_main_window.h"
 #include <QDebug>
+#include <QFile>
 
 main_window::main_window(QWidget *parent)
     : QMainWindow(parent),
     ui{new Ui::MainWindow()}
 {
     ui->setupUi(this);
+
+
+    QFile qss(":/styles/style.qss");
+    qss.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(qss.readAll());
+    setStyleSheet(styleSheet);
+
 }
 
 main_window::~main_window()
