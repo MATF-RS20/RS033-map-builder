47f924e0c72bae3d49b5b309d02de946f8a4be38
Luka Sokolov <lukasokolov@gmail.com>
2019-11-14 16:28:41 +0100

    Added a QToolButton for asset categories.

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -2,6 +2,8 @@
 #include "ui_main_window.h"
 #include <QDebug>
 #include <QFile>
+#include <QMenu>
+#include <QAction>
 
 main_window::main_window(QWidget *parent)
     : QMainWindow(parent),
@@ -10,10 +12,17 @@ main_window::main_window(QWidget *parent)
     ui->setupUi(this);
 
 
+    // TODO: Make and comment function for reading and loading qss style.
     QFile qss(":/styles/style.qss");
     qss.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(qss.readAll());
     setStyleSheet(styleSheet);
+
+    // TODO: Make and comment function that makes menu bar/expandable button for assets.
+    QMenu* terrain_menu = new QMenu();
+    terrain_menu->addAction(QIcon(":/icons/images/icons/preview.png"), "Land");
+    ui->tool_btn_terrain->setMenu(terrain_menu);
+
 
 }
 
