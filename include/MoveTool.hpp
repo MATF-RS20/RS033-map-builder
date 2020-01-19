04911c34b417995e7a104e5a67fa35c884ec73b4
Pedja Trifunov <trifunov.pedja@gmail.com>
2020-01-20 00:15:43 +0100

    Added move tool

---
+++ include/MoveTool.hpp
@@ -0,0 +1,26 @@
+#ifndef MAP_MAKER_MOVETOOL_HPP
+#define MAP_MAKER_MOVETOOL_HPP
+
+#include "Tool.hpp"
+
+namespace map_builder
+{
+    class MoveTool : public Tool
+    {
+    public:
+        MoveTool(QLayout *configurationLayout, QObject *parent);
+
+        void startedDrag(controller::StateController *stateController, QGraphicsScene *scene, QPoint point,
+                         Qt::MouseButton button) override;
+
+        void draged(controller::StateController *stateController, QGraphicsScene *scene, QVector2D dragedDirection,
+                    Qt::MouseButtons button) override;
+
+        void endedDrag(controller::StateController *stateController, QGraphicsScene *scene, QPoint point,
+                       Qt::MouseButton button) override;
+
+        void clicked(controller::StateController *stateController, QGraphicsScene *scene, QPointF point,
+                     Qt::MouseButton button) override;
+    };
+}
+#endif //MAP_MAKER_MOVETOOL_HPP
